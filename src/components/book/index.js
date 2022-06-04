7813f6f1230b36a5d09d2a4532b2f0fc2c084a82
Daniel <wasdrfff@yandex.ru>
2022-06-04 11:14:19 +0500

    style in component book

--- src/components/book/index.js
+++ src/components/book/index.js
@@ -1,6 +1,6 @@
 export const Book = ({ author, name, img }) => {
   return (
-    <div className="flex flex-col w-52 h-72">
+    <div className="flex flex-col w-52 h-72 min-w-max min-h-72">
       <img className=" object-contain w-36 h-60" src={img} alt="kartinka" />
       <span className="text-center">{author}</span>
       <span className="text-center">{name}</span>
